63ee21387ddf9ffb9a4446e0d4246a52867b924d
stephaniemartyna <stephanie.martyna@edu.sait.ca>
2023-12-04 17:12:18 -0700

    first commit

---
+++ src/treeImplementation/BSTreeNode.java
@@ -0,0 +1,38 @@
+package treeImplementation;
+
+public class BSTreeNode<E> {
+
+    private E data;
+    private BSTreeNode<E> left;
+    private BSTreeNode<E> right;
+
+    public BSTreeNode(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public BSTreeNode<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(BSTreeNode<E> left) {
+        this.left = left;
+    }
+
+    public BSTreeNode<E> getRight() {
+        return right;
+    }
+
+    public void setRight(BSTreeNode<E> right) {
+        this.right = right;
+    }
+}
